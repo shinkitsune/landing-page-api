30d44231e87dc65e0748bc0420ff85ca702f8a98
Elton Andrade <eltonandradee@gmail.com>
2021-04-24 14:50:09 -0300

    backend landing-page-api

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0c7d53be65fb7f511b83424059410354'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '88121cb4-50ce-48d5-9ca3-b7c6c3e84c6f'
+};
